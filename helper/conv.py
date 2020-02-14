f399093f2eda11b3d84441fe3257e744b75b74d7
Kadir Yanık <eleqiac2@gmail.com>
2020-02-14 10:18:04 +0300

    [ADD]: image file converter and rgb image added

---
+++ helper/conv.py
@@ -0,0 +1,14 @@
+# This simple module converts image format
+import sys
+import Image
+
+if len(sys.argv) != 3:
+	print "Usage: " + sys.argv[0] + " <input-img> <output-img>"
+	exit()
+
+inputImg = sys.argv[1]
+outputImg = sys.argv[2]
+
+print "Converting " + inputImg + " to " + outputImg
+img = Image.open(inputImg)
+img.save(outputImg)
